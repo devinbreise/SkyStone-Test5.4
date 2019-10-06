a2529b1e0d08c8f123fcb82aaad5e73cd9dcd4ae
ryoon <ryoon0510@gmail.com>
2019-10-06 14:41:20 -0700

    linear op mode changed to op mode formatting

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestGrabberIdea2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestGrabberIdea2.java
@@ -1,14 +1,12 @@
+
 package org.firstinspires.ftc.teamcode;
 
-
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "TestGrabberIdea2")
-
-public class TestGrabberIdea2 extends LinearOpMode {
+public class TestGrabberIdea2 extends OpMode {
     public static final double ROTATE_GRABBER_INCREMENT = 0.1;
 
 
@@ -19,31 +17,29 @@ public class TestGrabberIdea2 extends LinearOpMode {
 
     public static double GRABBER_ONE_OPEN_POS = 0.5;
     public static double GRABBER_TWO_OPEN_POS = 0.5;
-    public static int ROTATE_GRABBER_INITIAL_POS = 0;
+
+    public static double ROTATE_GRABBER_INITIAL_POS = 0;
+    public static double CURRENT_POS;
 
 
     private Servo grabberOne;
     private Servo grabberTwo;
     private Servo rotateGrabber;
 
-    @Override
-    public void runOpMode() throws InterruptedException {
+
+        public void init() {
+            grabberOne = hardwareMap.servo.get("grabberOne");
+            grabberTwo = hardwareMap.servo.get("grabberTwo");
+            rotateGrabber = hardwareMap.servo.get("rotateGrabber");
 
 
-        grabberOne = hardwareMap.servo.get("grabberOne");
-        grabberTwo = hardwareMap.servo.get("grabberTwo");
-//        rotateGrabber = hardwareMap.servo.get("rotateGrabber");
+            grabberOne.setPosition(GRABBER_ONE_OPEN_POS);
+            grabberTwo.setPosition(GRABBER_TWO_OPEN_POS);
+            CURRENT_POS = ROTATE_GRABBER_INITIAL_POS;
+            rotateGrabber.setPosition(CURRENT_POS);
+        }
 
-
-        grabberOne.setPosition(GRABBER_ONE_OPEN_POS);
-        grabberTwo.setPosition(GRABBER_TWO_OPEN_POS);
-//        rotateGrabber.setPosition(ROTATE_GRABBER_INITIAL_POS);
-
-
-        waitForStart();
-
-        while (opModeIsActive()) {
-
+        public void loop() {
             if (gamepad2.a) {
                 grabberOne.setPosition(GRABBER_ONE_CLOSED_POS);
                 grabberTwo.setPosition(GRABBER_TWO_CLOSED_POS);
@@ -63,12 +59,22 @@ public class TestGrabberIdea2 extends LinearOpMode {
                 //open
             }
 
-//            if (gamepad2.x) {
-//                double position = ROTATE_GRABBER_INITIAL_POS + ROTATE_GRABBER_INCREMENT;
-//                rotateGrabber.setPosition(position);
-//                telemetry.addData("rotateGrabber: ", rotateGrabber.getPosition());
-//
-//            }
+            if (gamepad2.x) {
+                double position = CURRENT_POS + ROTATE_GRABBER_INCREMENT;
+                rotateGrabber.setPosition(position);
+                CURRENT_POS = position;
+                telemetry.addData("rotateGrabber: ", rotateGrabber.getPosition());
+
+            }
+
+            if (gamepad2.y) {
+                double position = CURRENT_POS - ROTATE_GRABBER_INCREMENT;
+                rotateGrabber.setPosition(position);
+                CURRENT_POS = position;
+                telemetry.addData("rotateGrabber: ", rotateGrabber.getPosition());
+
+            }
+
 
             telemetry.update();
 
@@ -77,4 +83,4 @@ public class TestGrabberIdea2 extends LinearOpMode {
     }
 
 
-}
+
