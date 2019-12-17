a52b05b2fea6f0ef7cb4f94f6147aa226b0595ed
devinbreise <1loverobots!>
2019-12-16 16:47:25 -0800

    Changes made at the first match

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompetitionCode/AutoFoundationPathBlueCheck.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompetitionCode/AutoFoundationPathBlueCheck.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.teamcode.CompetitionCode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.Assemblies.Grabber;
@@ -11,9 +10,8 @@ import org.firstinspires.ftc.teamcode.Assemblies.LiftSystem;
 import org.firstinspires.ftc.teamcode.Assemblies.Robot;
 import org.firstinspires.ftc.teamcode.Assemblies.RobotDrive;
 import org.firstinspires.ftc.teamcode.basicLibs.teamUtil;
-@Autonomous(name="AutoFoundationPathRed", group ="Competition")
-
-public class AutoFoundationPathRed extends LinearOpMode {
+@Autonomous(name="AutoFoundationPathBlueCheck", group ="Competition")
+public class AutoFoundationPathBlueCheck extends LinearOpMode {
 
     Robot robot;
 
@@ -25,6 +23,7 @@ public class AutoFoundationPathRed extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
+
         telemetry.addLine("Initializing Op Mode");
         telemetry.update();
         initialize();
@@ -36,29 +35,30 @@ public class AutoFoundationPathRed extends LinearOpMode {
 
 
 
-        robot.drive.moveInchesLeft(0.5, 11, 4000);
+        telemetry.addLine("Ready to Start");
+        telemetry.update();
+        waitForStart();
 
-            robot.drive.moveInchesBackward(0.5,32,5000);
-            robot.latch.latchDown();
-            teamUtil.sleep(750);
-            robot.drive.moveInchesForward(0.5,37,6000);
-            robot.latch.latchUp();
+
+        robot.drive.moveInchesRight(0.5, 11, 4000);
+        robot.drive.moveInchesBackward(0.5,32,5000);
+        robot.latch.latchDown();
+        teamUtil.sleep(750);
+
+        robot.drive.moveInchesForward(0.5,37,6000);
+        robot.latch.latchUp();
         teamUtil.sleep(1000);
 
-        robot.drive.moveInchesRight(0.5, 12,5000);
-            robot.drive.moveInchesBackward(0.5,0.25, 2000);
-            robot.drive.moveInchesRight(0.5, 38,5000);
+        robot.drive.moveInchesLeft(0.5, 12,5000);
+        robot.drive.moveInchesBackward(0.5,0.25,2000);
 
-//TODO: added code
-            robot.drive.moveInchesForward(0.5, 7, 3000);
+        robot.drive.moveInchesLeft(0.5, 38,5000);
 
 
-
-
-//            robot.drive.moveInchesBackward(0.5,20,5000);
-//            robot.drive.moveInchesLeft(0.5, 24,5000);
-//            robot.drive.moveInchesForward(0.5, 20,5000);
-//            robot.drive.moveInchesRight(0.5, 32,5000);
+        robot.drive.moveInchesBackward(0.5,20,5000);
+        robot.drive.moveInchesRight(0.5, 27,5000);
+        robot.drive.moveInchesForward(0.5, 25,5000);
+        robot.drive.moveInchesLeft(0.5, 32,5000);
 
 
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompetitionCode/AutoFoundationPathRedCheck.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompetitionCode/AutoFoundationPathRedCheck.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.teamcode.CompetitionCode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.Assemblies.Grabber;
@@ -11,9 +10,8 @@ import org.firstinspires.ftc.teamcode.Assemblies.LiftSystem;
 import org.firstinspires.ftc.teamcode.Assemblies.Robot;
 import org.firstinspires.ftc.teamcode.Assemblies.RobotDrive;
 import org.firstinspires.ftc.teamcode.basicLibs.teamUtil;
-@Autonomous(name="AutoFoundationPathRed", group ="Competition")
-
-public class AutoFoundationPathRed extends LinearOpMode {
+@Autonomous(name="AutoFoundationPathRedCheck", group ="Competition")
+public class AutoFoundationPathRedCheck extends LinearOpMode {
 
     Robot robot;
 
@@ -38,27 +36,20 @@ public class AutoFoundationPathRed extends LinearOpMode {
 
         robot.drive.moveInchesLeft(0.5, 11, 4000);
 
-            robot.drive.moveInchesBackward(0.5,32,5000);
-            robot.latch.latchDown();
-            teamUtil.sleep(750);
-            robot.drive.moveInchesForward(0.5,37,6000);
-            robot.latch.latchUp();
+        robot.drive.moveInchesBackward(0.5,32,5000);
+        robot.latch.latchDown();
+        teamUtil.sleep(750);
+        robot.drive.moveInchesForward(0.5,37,6000);
+        robot.latch.latchUp();
         teamUtil.sleep(1000);
 
         robot.drive.moveInchesRight(0.5, 12,5000);
-            robot.drive.moveInchesBackward(0.5,0.25, 2000);
-            robot.drive.moveInchesRight(0.5, 38,5000);
-
-//TODO: added code
-            robot.drive.moveInchesForward(0.5, 7, 3000);
-
-
-
-
-//            robot.drive.moveInchesBackward(0.5,20,5000);
-//            robot.drive.moveInchesLeft(0.5, 24,5000);
-//            robot.drive.moveInchesForward(0.5, 20,5000);
-//            robot.drive.moveInchesRight(0.5, 32,5000);
+        robot.drive.moveInchesBackward(0.5,0.5, 5000);
+        robot.drive.moveInchesRight(0.5, 28,5000);
+        robot.drive.moveInchesBackward(0.5,20,5000);
+        robot.drive.moveInchesLeft(0.5, 24,5000);
+        robot.drive.moveInchesForward(0.5, 20,5000);
+        robot.drive.moveInchesRight(0.5, 32,5000);
 
 
 
